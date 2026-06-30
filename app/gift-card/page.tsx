2116503989edd443fcf655d42943cfa146a6175e
Aqsa <aqsadar64@gmail.com>
2026-06-30 20:10:41 +0500

    suspend boundary for gift-card to bypass build failure issue

--- app/gift-card/page.tsx
+++ app/gift-card/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import { fetchOrganization } from '@/lib/api/serverInit'
 import { getDomain } from '@/lib/utils/getDomain'
 import { buildPageMetadata } from '@/lib/utils/metaTags'
@@ -15,7 +16,9 @@ export default async function GiftCardPage() {
   return (
     <div>
       <LandingPageBanner component="LandingPageBanner" headline="Gift Cards" />
-      <GiftCard />
+      <Suspense>
+        <GiftCard />
+      </Suspense>
     </div>
   )
 }
